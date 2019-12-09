492bb5736015a7df00f39d08b8d1c0d5cd85e170
Tom Wright <tomnwright@outlook.com>
2019-12-09 00:46:53 +0000

    New - working?
    Structural features are working. Generation algorithm cannot get above 73 (out of 81) squares. It always gets to a point where its impossible to put any more numbers in. How to fix this? Also, if I find a working algorithm, how will I determine the minimum amount of information required for a human to solve each grid?

--- sudoku-py.py
+++ sudoku-py.py
@@ -2,15 +2,16 @@ import random
 
 class s9Grid:
     def __init__(self):
-        self.grid = [[i * j for j in range(9)] for i in range(9)]
+        self.grid = [[' ' for j in range(9)] for i in range(9)]
 
     def __iter__(self):
         return [row for row in self.grid]
 
     def __getitem__(self, key):
-        return self.grid[key[0]][key[1]]
+        '''key = (x,y,)'''
+        return self.grid[key[1]][key[0]]
     def __setitem__(self, key, value):
-        self.grid[key[0]][key[1]] = value
+        self.grid[key[1]][key[0]] = value
     def __str__(self):
         out = ''
         
@@ -30,32 +31,93 @@ class s9Grid:
     def solved (self):
         for i in self.grid:
             for j in i:
-                if j == 0:
+                if j == ' ':
                     return False
         return True
 
-    def sudokuFormat (self):
-        
-        out = []
-        for i in range(9):
-            #cells
-            cell = []
-            for j in range(9):
-                #elements
-                y =  3 * (i // 3)  +  3 * (j // 3)
-                x =  3 * (i % 3)   + 3 * (j // 3)
-                cell += self[y,x,]
-            out += cell
+    def getCell (self, i):
+        cell = []
+        for j in range(9):
+            #elements
+            x =  3 * (i % 3) + (j % 3)
+            y =  3 * (i // 3) + (j // 3)
+
+            cell.append(self[x,y])
         return cell
-        
+    
+    def getRow (self, row):
+
+        return self.grid[row]
+    
+    def getColumn (self, column):
+
+        return [row[column] for row in self.grid]
+
+    def CanGo (self, key, value):
+        b1 = value in self.getCell(s9Grid.cellNumber(*key))
+        b2 = value in self.getColumn(key[0])
+        b3 = value in self.getRow(key[1])
+        return not (b1 or b2 or b3)
+
+    @staticmethod
+    def cellNumber(x, y):
+        return 3 * (y // 3) + (x // 3)
+        #j = 3 * (y % 3) + (x % 3), see getCell
 
 
-grid = s9Grid()
+def testKey(grid, key):
+    numbers = list(range(1,10))
 
-keys = [[(i,j,) for j in range(9)] for i in range(9)]
-'''
-while not grid.solved():
-    pass
-'''
-print(grid)
-print(grid.sudokuFormat())
+    while (numbers):
+
+        val = random.choice(numbers)
+        numbers.remove(val)
+
+        if grid.CanGo(key,val):
+            return val
+
+    return None
+
+
+#define all keys
+keysRaw = []
+for i in range(9):
+    for j in range(9):
+        k = (i,j,)
+        keysRaw.append(k)
+
+count = 0
+best = 0
+while True:
+
+    grid = s9Grid()
+    keys = keysRaw.copy()
+     
+    buffer = []
+    success_count = 0
+    while not grid.solved():
+        key = random.choice(keys)
+        if key in buffer:
+            continue
+
+        test = testKey(grid,key)
+        if (test):
+            
+            success_count += 1
+            grid[key] = test
+            buffer = []
+            keys.remove(key)
+
+        else:
+
+            buffer.append(key)
+            if set(buffer) == set(keys):
+                break
+    count += 1
+    print(grid, end = '\n\n\n')
+    
+    
+    if grid.solved() or count > 50:
+        break
+print('SOLVED')
+input()
